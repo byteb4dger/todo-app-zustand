abc6150f44c82c50f0b0e86093e78cfaa326439a
Piotr Raducha <gulaszewski.piotr@gmail.com>
2023-12-03 21:41:11 +0100

    Create Title.tsx

---
+++ src/app/components/Title.tsx
@@ -0,0 +1,10 @@
+const Title = () => {
+  return (
+    <header className="flex flex-col pb-16">
+      <h1 className="text-5xl leading-relaxed">Todo App</h1>{" "}
+      <p className="text-2xl leading-relaxed">by Piotr Raducha</p>
+    </header>
+  );
+};
+
+export default Title;
