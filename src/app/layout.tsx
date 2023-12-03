28c1bc601c57aa0af1b35fc7b5cfb047ee6ebf35
Piotr Raducha <gulaszewski.piotr@gmail.com>
2023-12-03 21:41:02 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Title from "./components/title/page";
+import Title from "./components/Title";
 
 const inter = Inter({ subsets: ["latin"] });
 
